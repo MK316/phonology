f59709730712b610dbce14e40e1a97d83330783e
MK <99416359+MK316@users.noreply.github.com>
2025-12-30 18:52:54 +0900

    Update Message_Board.py

--- pages/Message_Board.py
+++ pages/Message_Board.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("🐳 English Linguistics Study Lab: Phonetics, Phonology & Morphology Study Guides")
+def main():
+    st.caption("💙 Greetings! Feel free to leave any feedback, suggestions, or messages about the application on this page. I'll make sure to look into them as soon as I can! 😍")
+    st.write("➡️ Click the '+' sign to write.")
+    # Padlet embed URL (you need to replace this with your actual Padlet embed URL)
+    padlet_url = "https://padlet.com/mirankim316/englinglab"
+
+    # Create an iframe to embed the Padlet
+    padlet_iframe = f"<iframe src='{padlet_url}' width='100%' height='600' frameborder='0' allow='autoplay'></iframe>"
+
+    # Display the iframe in Streamlit
+    components.html(padlet_iframe, height=600)
+
+if __name__ == "__main__":
+    main()
