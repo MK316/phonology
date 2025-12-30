4058bbad4d26249b6ad3da838dd8a398f309bc3e
MK <99416359+MK316@users.noreply.github.com>
2025-12-30 20:14:38 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.markdown("#### 🐳 English Linguistics Study Lab: ")
 st.write("Phonetics, Phonology & Morphology Study Guides")
+
+st.caption("Preparing for the TCExam 2027")
+st.caption("Starting from Dec.30, 2025")
