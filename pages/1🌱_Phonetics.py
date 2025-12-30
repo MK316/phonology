bcd80a1b9c123f2d7d2ca7a6560cca80d20556fd
MK <99416359+MK316@users.noreply.github.com>
2025-12-30 19:29:11 +0900

    Update 1🌱_Phonetics.py

--- pages/1🌱_Phonetics.py
+++ pages/1🌱_Phonetics.py
@@ -5,3 +5,10 @@ st.write("This section provides a study guide for English phonetics. A solid fou
 
 st.markdown("---")
 st.markdown("1. Foundational reading: Work through the selected chapters in the textbook below, emphasizing comprehension of core ideas.")
+
+st.write"📗 Textbook1: A course in phonetics""
+st.markdown("""📍Notes:
+
+    1. You'll need a passcode to open each pdf file.
+    2. Each chapter highlights key terms. As you read, we recommend briefly considering why these terms are highlighted. Some of them represent core concepts and should be studied with careful attention to their definitions and examples.")
+    """)
