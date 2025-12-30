3fc45f0048e4513891b7d064cc29abaa51337c62
MK <99416359+MK316@users.noreply.github.com>
2025-12-30 19:11:18 +0900

    Update 1🌱_Phonetics.py

--- pages/1🌱_Phonetics.py
+++ pages/1🌱_Phonetics.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.write("Phonetics study guide")
+st.markdown("#### Phonetics study guide")
+st.write("This section provides a study guide for English phonetics. A solid foundation in phonetics is essential for approaching phonology and more advanced English linguistics questions in the teacher certification examination.")
