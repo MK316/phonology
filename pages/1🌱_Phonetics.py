d95dbc9dbd2bf848abe30057c4d3c220b8fe9efa
MK <99416359+MK316@users.noreply.github.com>
2025-12-30 20:19:06 +0900

    Update 1🌱_Phonetics.py

--- pages/1🌱_Phonetics.py
+++ pages/1🌱_Phonetics.py
@@ -1,36 +1,49 @@
 import streamlit as st
 
-st.markdown("### Phonetics Study Guide")
-st.write(
-    "This section provides a study guide for English phonetics. "
-    "A solid foundation in phonetics is essential for approaching phonology "
-    "and more advanced English linguistics questions in the teacher certification examination."
-)
+tab1, tab2, tab3, tab4 = st.tabs(
+    ["📘 Basic readings", "🍰 TBA", "🍰 TBA", "🍰 Practice"]
 
-st.markdown("---")
+with tab1:
+    
+    st.markdown("### Phonetics Study Guide")
+    st.write(
+        "This section provides a study guide for English phonetics. "
+        "A solid foundation in phonetics is essential for approaching phonology "
+        "and more advanced English linguistics questions in the teacher certification examination."
+    )
+    
+    st.markdown("---")
+    
+    st.markdown(
+        "#### 1. Foundational reading")
+    st.caption("Work through the selected chapters in the textbook below, "
+        "emphasizing comprehension of core ideas."
+    )
+    
+    st.write("📗 **Textbook 1:** *A Course in Phonetics (7th edition)*")
+    
+    st.markdown("""📍Notes:
+    
+    1. You'll need a passcode to open each PDF file.
+    2. Each chapter highlights key terms. As you read, I recommend briefly considering why these terms are highlighted. Some of them represent core concepts and should be studied with careful attention to their definitions and examples.
+    """)
+    
+    st.markdown("""
+    | Chapter | Topic                         | Link |
+    |--------|-------------------------------|------|
+    | 1      | Articulation and Acoustics     | [📁](https://drive.google.com/file/d/1F6ZDp-AHFPRB7mjj9SsVjA1bMd2kpjWM/view?usp=sharing)  |
+    | 2      | Phonology and Phonetic Transcription  | [📁](https://drive.google.com/file/d/1ColrqCqVW_euaXxh48V36uDSRuBcJV0f/view?usp=drive_link)  |
+    | 3      | The Consonants of English                  | [📁](https://drive.google.com/file/d/10bq89qxlxuvyeUPh5p_cJpPj79Db0jBU/view?usp=drive_link)  |
+    | 4      | English Vowels                        | [📁](https://drive.google.com/file/d/1A2jvf4zRBhnVjTYAUH5zsIWNuPP_TqT9/view?usp=drive_link)  |
+    | 5      | English Words and Sentences         | [📁](https://drive.google.com/file/d/1y6bKgR4CoG5oHEQ2kHElyvLs-BIL16_c/view?usp=drive_link)  |
+    """)
 
-st.markdown(
-    "#### 1. Foundational reading")
-st.caption("Work through the selected chapters in the textbook below, "
-    "emphasizing comprehension of core ideas."
-)
+with tab2:
+    st.write("TBA")
 
-st.write("📗 **Textbook 1:** *A Course in Phonetics (7th edition)*")
+with tab3:
+    st.write("TBA")
 
-st.markdown("""📍Notes:
-
-1. You'll need a passcode to open each PDF file.
-2. Each chapter highlights key terms. As you read, I recommend briefly considering why these terms are highlighted. Some of them represent core concepts and should be studied with careful attention to their definitions and examples.
-""")
-
-st.markdown("""
-| Chapter | Topic                         | Link |
-|--------|-------------------------------|------|
-| 1      | Articulation and Acoustics     | [📁](https://drive.google.com/file/d/1F6ZDp-AHFPRB7mjj9SsVjA1bMd2kpjWM/view?usp=sharing)  |
-| 2      | Phonology and Phonetic Transcription  | [📁](https://drive.google.com/file/d/1ColrqCqVW_euaXxh48V36uDSRuBcJV0f/view?usp=drive_link)  |
-| 3      | The Consonants of English                  | [📁](https://drive.google.com/file/d/10bq89qxlxuvyeUPh5p_cJpPj79Db0jBU/view?usp=drive_link)  |
-| 4      | English Vowels                        | [📁](https://drive.google.com/file/d/1A2jvf4zRBhnVjTYAUH5zsIWNuPP_TqT9/view?usp=drive_link)  |
-| 5      | English Words and Sentences         | [📁](https://drive.google.com/file/d/1y6bKgR4CoG5oHEQ2kHElyvLs-BIL16_c/view?usp=drive_link)  |
-""")
-
+with tab4:
+    st.write("TBA")
 
